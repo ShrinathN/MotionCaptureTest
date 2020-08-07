86252c084be8eb15ba2205fba1e6ea8732812bbd
Shrinath Nimare <shrinath.nimare@gmail.com>
2020-08-07 15:11:52 +0530

    Starting Engine

---
+++ renderer/common_includes.h
@@ -0,0 +1,13 @@
+#ifndef COMMON_INCLUDES_H
+#define COMMON_INCLUDES_H
+
+#include <GLFW/glfw3.h>
+#include <stb_image.h>
+#include <unistd.h>
+#include <math.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#endif
